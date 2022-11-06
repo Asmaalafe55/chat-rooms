b1fff5bee4dc86218d10285345be127d9ad03179
Asmaalafe55 <asmaalafe55@gmail.com>
2022-11-06 19:48:35 +0200

    editing

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-//Set static folder
+// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+const botName = 'Admin';
+
+// Run when client connects
+io.on('connection', (socket) => {
+  // Welcome current user (This is to single client)
+  socket.emit('message', formatMessage(botName, 'Welcome to ChatRooms!'));
+
+  // Brodcast when a user connects (This is to all clients except the client that connecting)
+  socket.broadcast.emit(
+    'message',
+    formatMessage(botName, 'A user has joined the chat')
+  );
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    // This to all clients in general
+    io.emit('message', formatMessage(botName, 'A user has left the chat'));
+  });
+
+  // Listen for chatMessage
+  socket.on('chatMessage', (msg) => {
+    io.emit('message', formatMessage('USER', msg));
+  });
+});
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
